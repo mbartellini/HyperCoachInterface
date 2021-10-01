682fb27d2de22ad71d3530ff9c2f4251a10f0e2a
BraveJero <jbrave271@gmail.com>
2021-09-30 23:19:24 -0300

    add basic card structure

---
+++ src/components/RoutineCard.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-card
+        elevation="6"
+        max-width="300"
+        class="rounded-card"
+    >
+      <v-img
+          :src="img_src"
+          height="170px"
+      ></v-img>  <!-- Make it variable so that it does not break in small screens. -->
+      <v-card-title class = "justify-center">
+        <slot>None</slot>
+      </v-card-title>
+    </v-card>
+</template>
+
+<script>
+export default {
+  name: "RoutineCard",
+  props: ['img_src']
+}
+</script>
+
+<style scoped>
+.rounded-card {
+  border-radius: 10px;
+  margin: 10px;
+}
+</style>
