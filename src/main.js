2ca712a76ee791297d9f9a5769b80c4092cfb191
BraveJero <jbrave271@gmail.com>
2021-10-06 15:58:27 -0300

    add vuex to main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 
+import Vuesax from 'vuesax'
+
+import 'vuesax/dist/vuesax.css' //Vuesax styles
+
 Vue.config.productionTip = false
+
+Vue.use(Vuesax, {
+  // options here
+})
 
 new Vue({
   vuetify,
