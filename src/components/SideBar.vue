8a74d5aa727adeae7f30922eb438ca950dfd757a
BraveJero <jbrave271@gmail.com>
2021-10-06 15:56:29 -0300

    add basic sidebar.

---
+++ src/components/SideBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hidden">
+    <vs-sidebar
+        background="dark"
+        textWhite
+        absolute
+        v-model="active"
+        open
+    >
+      <vs-sidebar-item id="placeHolder1">
+        placeHolder1
+      </vs-sidebar-item>
+      <vs-sidebar-item id="placeHolder2">
+        placeHolder2
+      </vs-sidebar-item>
+      <vs-sidebar-item id="placeHolder3">
+        placeHolder3
+      </vs-sidebar-item>
+      <vs-sidebar-item id="placeHolder4">
+        placeHolder4
+      </vs-sidebar-item>
+    </vs-sidebar>
+  </div>
+</template>
+
+<script>
+export default {
+  data:() => ({
+    active: 'home',
+  })
+}
+</script>
