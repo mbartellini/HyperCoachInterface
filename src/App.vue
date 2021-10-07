ef8f64adfb6e6caad573eee6c6ba65309c9a6b30
juan <juanigarcia@itba.edu.ar>
2021-10-07 12:30:48 -0300

    Add views to app.

--- src/App.vue
+++ src/App.vue
@@ -37,20 +37,23 @@
       </v-btn>
     </v-app-bar>
 
+
     <v-main>
-      <HelloWorld/>
+        <Home/>
     </v-main>
+
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Home from './views/Home';
+
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Home,
   },
 
   data: () => ({
