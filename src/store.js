fdf39b138f9083944278c8f0cce7cb85a0a59c23
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2021-10-10 10:08:50 -0300

    Create store with testing objects

---
+++ src/store.js
@@ -0,0 +1,474 @@
+export default {
+    routines: [
+        {
+            id: 0,
+            title: 'Rutina 0',
+            img_src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg',
+            description: {
+                duration: 60,
+                difficulty: 'Principiante',
+                likes: 7700,
+                goal: 'Tonificar músculos',
+                muscles: 'Piernas',
+                equipment: true,
+                favourite: false,
+            },
+            cycles: [
+                {
+                    title: 'Ciclo 0',
+                    exercises: [
+                        {
+                            title: 'Ejercicio 0',
+                            duration: 15,
+                            repetitions: null,
+                        },
+                        {
+                            title: 'Ejercicio 1',
+                            duration: null,
+                            repetitions: 20,
+                        },
+                        {
+                            title: 'Ejercicio 2',
+                            duration: 15,
+                            repetitions: null,
+                        },
+                        {
+                            title: 'Ejercicio 3',
+                            duration: null,
+                            repetitions: 20,
+                        },
+                    ],
+
+                },
+                {
+                    title: 'Ciclo 1',
+                    exercises: [
+                        {
+                            title: 'Ejercicio 0',
+                            duration: 15,
+                            repetitions: null,
+                        },
+                        {
+                            title: 'Ejercicio 1',
+                            duration: null,
+                            repetitions: 20,
+                        },
+                        {
+                            title: 'Ejercicio 2',
+                            duration: 15,
+                            repetitions: null,
+                        },
+                        {
+                            title: 'Ejercicio 3',
+                            duration: null,
+                            repetitions: 20,
+                        },
+                    ],
+
+                },
+                {
+                    title: 'Ciclo 2',
+                    exercises: [
+                        {
+                            title: 'Ejercicio 0',
+                            duration: 15,
+                            repetitions: null,
+                        },
+                        {
+                            title: 'Ejercicio 1',
+                            duration: null,
+                            repetitions: 20,
+                        },
+                        {
+                            title: 'Ejercicio 2',
+                            duration: 15,
+                            repetitions: null,
+                        },
+                        {
+                            title: 'Ejercicio 3',
+                            duration: null,
+                            repetitions: 20,
+                        },
+                    ],
+
+                },
+            ],
+        },
+        {
+            id: 1,
+            title: 'Rutina 1',
+            img_src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg',
+            description: {
+                duration: 60,
+                difficulty: 'Principiante',
+                likes: 7700,
+                goal: 'Tonificar músculos',
+                muscles: 'Piernas',
+                equipment: true,
+                favourite: false,
+            },
+            cycles: [
+                {
+                    title: 'Ciclo 0',
+                    exercises: [
+                        {
+                            title: 'Ejercicio 0',
+                            duration: 15,
+                            repetitions: null,
+                        },
+                        {
+                            title: 'Ejercicio 1',
+                            duration: null,
+                            repetitions: 20,
+                        },
+                        {
+                            title: 'Ejercicio 2',
+                            duration: 15,
+                            repetitions: null,
+                        },
+                        {
+                            title: 'Ejercicio 3',
+                            duration: null,
+                            repetitions: 20,
+                        },
+                    ],
+
+                },
+                {
+                    title: 'Ciclo 1',
+                    exercises: [
+                        {
+                            title: 'Ejercicio 0',
+                            duration: 15,
+                            repetitions: null,
+                        },
+                        {
+                            title: 'Ejercicio 1',
+                            duration: null,
+                            repetitions: 20,
+                        },
+                        {
+                            title: 'Ejercicio 2',
+                            duration: 15,
+                            repetitions: null,
+                        },
+                        {
+                            title: 'Ejercicio 3',
+                            duration: null,
+                            repetitions: 20,
+                        },
+                    ],
+
+                },
+                {
+                    title: 'Ciclo 2',
+                    exercises: [
+                        {
+                            title: 'Ejercicio 0',
+                            duration: 15,
+                            repetitions: null,
+                        },
+                        {
+                            title: 'Ejercicio 1',
+                            duration: null,
+                            repetitions: 20,
+                        },
+                        {
+                            title: 'Ejercicio 2',
+                            duration: 15,
+                            repetitions: null,
+                        },
+                        {
+                            title: 'Ejercicio 3',
+                            duration: null,
+                            repetitions: 20,
+                        },
+                    ],
+
+                },
+            ],
+        },
+        {
+            id: 2,
+            title: 'Rutina 2',
+            img_src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg',
+            description: {
+                duration: 60,
+                difficulty: 'Principiante',
+                likes: 7700,
+                goal: 'Tonificar músculos',
+                muscles: 'Piernas',
+                equipment: true,
+                favourite: false,
+            },
+            cycles: [
+                {
+                    title: 'Ciclo 0',
+                    exercises: [
+                        {
+                            title: 'Ejercicio 0',
+                            duration: 15,
+                            repetitions: null,
+                        },
+                        {
+                            title: 'Ejercicio 1',
+                            duration: null,
+                            repetitions: 20,
+                        },
+                        {
+                            title: 'Ejercicio 2',
+                            duration: 15,
+                            repetitions: null,
+                        },
+                        {
+                            title: 'Ejercicio 3',
+                            duration: null,
+                            repetitions: 20,
+                        },
+                    ],
+
+                },
+                {
+                    title: 'Ciclo 1',
+                    exercises: [
+                        {
+                            title: 'Ejercicio 0',
+                            duration: 15,
+                            repetitions: null,
+                        },
+                        {
+                            title: 'Ejercicio 1',
+                            duration: null,
+                            repetitions: 20,
+                        },
+                        {
+                            title: 'Ejercicio 2',
+                            duration: 15,
+                            repetitions: null,
+                        },
+                        {
+                            title: 'Ejercicio 3',
+                            duration: null,
+                            repetitions: 20,
+                        },
+                    ],
+
+                },
+                {
+                    title: 'Ciclo 2',
+                    exercises: [
+                        {
+                            title: 'Ejercicio 0',
+                            duration: 15,
+                            repetitions: null,
+                        },
+                        {
+                            title: 'Ejercicio 1',
+                            duration: null,
+                            repetitions: 20,
+                        },
+                        {
+                            title: 'Ejercicio 2',
+                            duration: 15,
+                            repetitions: null,
+                        },
+                        {
+                            title: 'Ejercicio 3',
+                            duration: null,
+                            repetitions: 20,
+                        },
+                    ],
+
+                },
+            ],
+        },
+        {
+            id: 3,
+            title: 'Rutina 3',
+            img_src: 'https://i.pinimg.com/originals/25/49/82/25498264b4b0e7bd98587789c0e4ffaa.jpg',
+            description: {
+                duration: 60,
+                difficulty: 'Principiante',
+                likes: 7700,
+                goal: 'Tonificar músculos',
+                muscles: 'Piernas',
+                equipment: true,
+                favourite: false,
+            },
+            cycles: [
+                {
+                    title: 'Ciclo 0',
+                    exercises: [
+                        {
+                            title: 'Ejercicio 0',
+                            duration: 15,
+                            repetitions: null,
+                        },
+                        {
+                            title: 'Ejercicio 1',
+                            duration: null,
+                            repetitions: 20,
+                        },
+                        {
+                            title: 'Ejercicio 2',
+                            duration: 15,
+                            repetitions: null,
+                        },
+                        {
+                            title: 'Ejercicio 3',
+                            duration: null,
+                            repetitions: 20,
+                        },
+                    ],
+
+                },
+                {
+                    title: 'Ciclo 1',
+                    exercises: [
+                        {
+                            title: 'Ejercicio 0',
+                            duration: 15,
+                            repetitions: null,
+                        },
+                        {
+                            title: 'Ejercicio 1',
+                            duration: null,
+                            repetitions: 20,
+                        },
+                        {
+                            title: 'Ejercicio 2',
+                            duration: 15,
+                            repetitions: null,
+                        },
+                        {
+                            title: 'Ejercicio 3',
+                            duration: null,
+                            repetitions: 20,
+                        },
+                    ],
+
+                },
+                {
+                    title: 'Ciclo 2',
+                    exercises: [
+                        {
+                            title: 'Ejercicio 0',
+                            duration: 15,
+                            repetitions: null,
+                        },
+                        {
+                            title: 'Ejercicio 1',
+                            duration: null,
+                            repetitions: 20,
+                        },
+                        {
+                            title: 'Ejercicio 2',
+                            duration: 15,
+                            repetitions: null,
+                        },
+                        {
+                            title: 'Ejercicio 3',
+                            duration: null,
+                            repetitions: 20,
+                        },
+                    ],
+
+                },
+            ],
+        },
+        {
+            id: 4,
+            title: 'Rutina 4',
+            img_src: '../assets/arms.jpg',
+            description: {
+                duration: 60,
+                difficulty: 'Principiante',
+                likes: 7700,
+                goal: 'Tonificar músculos',
+                muscles: 'Piernas',
+                equipment: true,
+                favourite: false,
+            },
+            cycles: [
+                {
+                    title: 'Ciclo 0',
+                    exercises: [
+                        {
+                            title: 'Ejercicio 0',
+                            duration: 15,
+                            repetitions: null,
+                        },
+                        {
+                            title: 'Ejercicio 1',
+                            duration: null,
+                            repetitions: 20,
+                        },
+                        {
+                            title: 'Ejercicio 2',
+                            duration: 15,
+                            repetitions: null,
+                        },
+                        {
+                            title: 'Ejercicio 3',
+                            duration: null,
+                            repetitions: 20,
+                        },
+                    ],
+
+                },
+                {
+                    title: 'Ciclo 1',
+                    exercises: [
+                        {
+                            title: 'Ejercicio 0',
+                            duration: 15,
+                            repetitions: null,
+                        },
+                        {
+                            title: 'Ejercicio 1',
+                            duration: null,
+                            repetitions: 20,
+                        },
+                        {
+                            title: 'Ejercicio 2',
+                            duration: 15,
+                            repetitions: null,
+                        },
+                        {
+                            title: 'Ejercicio 3',
+                            duration: null,
+                            repetitions: 20,
+                        },
+                    ],
+
+                },
+                {
+                    title: 'Ciclo 2',
+                    exercises: [
+                        {
+                            title: 'Ejercicio 0',
+                            duration: 15,
+                            repetitions: null,
+                        },
+                        {
+                            title: 'Ejercicio 1',
+                            duration: null,
+                            repetitions: 20,
+                        },
+                        {
+                            title: 'Ejercicio 2',
+                            duration: 15,
+                            repetitions: null,
+                        },
+                        {
+                            title: 'Ejercicio 3',
+                            duration: null,
+                            repetitions: 20,
+                        },
+                    ],
+
+                },
+            ],
+        },
+    ],
+}
