fa1741c5a4f79b7cc46326802d618300870f97e2
BraveJero <jbrave271@gmail.com>
2021-10-10 09:31:10 -0300

    add api

---
+++ sportAPI/config/mailing.js
@@ -0,0 +1,31 @@
+// https://ethereal.email/
+module.exports = {
+    service: {
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false,
+        user: 'alan37@ethereal.email',
+        password: 'afF9kEaXSJ6KFRgKxC'
+    },
+    registerVerification: {
+        from: 'Exercise <noreply@exercise.com>',
+        subject: 'Email Confirmation',
+        htmlBodyTemplate:
+            `<div>
+                <h1 style="text-align: center;">
+                    <strong>Welcome to <span style="color: #fc987e;">Exercise</span></strong>
+                </h1>
+                <p>
+                    <span>Valid until <%EXPIRATION_DATE%></span>
+                </p>
+                <div>
+                    <a href="<%CONFIRM_LINK%>">Click para confirmar</a>
+                <div>
+                <h1 style="text-align: center;">
+                    <strong>Your code is <span style="color: #fc987e;"><%CODE%></span></strong>
+                </h1>
+            </div>`,
+        confirmationLink: 'http://localhost:8080/verification?user=<%USER_ID%>&code=<%CODE%>',
+        codeExpirationInterval: 24 * 60 * 60 * 1000
+    }
+}
