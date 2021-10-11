6a5fe2b495ff41b8e2fcff6671d110520d49ef52
juan <juanigarcia@itba.edu.ar>
2021-10-11 17:59:58 -0300

    Add MyRoutines view.

--- src/views/MyRoutines.vue
+++ src/views/MyRoutines.vue
@@ -1,17 +1,41 @@
 <template>
-  <h1>My Routines</h1>
+  <div>
+    <h1>MyRoutines</h1>
+    <v-container fluid >
+      <v-row dense >
+        <v-col v-for="routine in my_routines" :key="routine">
+          <RoutineCard :img_src="routine.src">{{routine.title}}</RoutineCard>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 
 
 <script>
+import RoutineCard from '@/components/RoutineCard'
+
 export default {
   name: 'MyRoutines',
 
   components: {
+    RoutineCard
   },
 
   data: () => ({
   }),
+
+  computed: {
+    my_routines() {
+      return [
+        { title: 'Abdominales', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg'},
+        { title: 'Tren Superior', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg'},
+        { title: 'Piernas', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg'},
+        { title: 'Yoga', src: 'https://i.pinimg.com/originals/25/49/82/25498264b4b0e7bd98587789c0e4ffaa.jpg'},
+        { title: 'Brazos', src: '../assets/arms.jpg' }
+      ]
+    }
+  }
 }
 </script>
