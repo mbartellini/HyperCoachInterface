feaa5925790c016d72489cef7087b31e2bc3aee0
juan <juanigarcia@itba.edu.ar>
2021-10-11 19:08:46 -0300

    Fix (?) login info in topbar.

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,24 +1,26 @@
 <template>
   <v-app-bar app clipped-left class="secondary align-center">
-    <v-img src="@/assets/hci.png" alt="Logo" :max-height="50" :max-width="50" />
+    <v-img src="@/assets/hci.png" alt="Logo" min-width="100" min-height="100" max-height="100" max-width="100"/>
     <!-- <v-app-bar-nav-icon v-if="!permanent" @click.stop="drawer = !drawer" /> -->
     <v-app-bar-title class="title" >Hyper Coach Interface</v-app-bar-title>
     <v-spacer/>
     <div v-if="username"> <!-- Logged in -->
-      <router-link class="profile" :to="{name: profileRoute}">
+      <router-link class="loginprompt" :to="{name: profileRoute}">
         {{username}}
       </router-link>
-      <figure >
-        <router-link class="profilepic" :to="{name: profileRoute}" >
-          <img :src="profile_pic" :height="32" :width="32" class="profilepic" alt="Profile picture"/>
-        </router-link>
-      </figure>
+<!--      <v-img class="profilepic" src="@/assets/Juani.jpeg" />-->
+
+<!--      <figure >-->
+<!--        <router-link class="profilepic" :to="{name: profileRoute}" >-->
+<!--          <img :src="profile_pic" :height="32" :width="32" class="profilepic" alt="Profile picture"/>-->
+<!--        </router-link>-->
+<!--      </figure>-->
     </div>
     <div v-else> <!-- Not logged in -->
-      <router-link :to="{name: 'Login'}">
+      <router-link class="loginprompt" :to="{name: 'Login'}">
         Iniciar sesión /
       </router-link>
-      <router-link :to="{name: 'Registration'}" >
+      <router-link class="loginprompt" :to="{name: 'Registration'}" >
         Registrarse
       </router-link>
     </div>
@@ -31,7 +33,7 @@ export default {
 
   data: () => ({
     profileRoute: 'Profile',
-    username: 'Juan',
+    username: 'juanigarcia',
     profile_pic: "@/assets/Juani.jpeg",
   }),
 }
@@ -41,22 +43,18 @@ export default {
 .title {
   color: white;
   font-weight: bolder;
-  padding: 10px;
-}
-
-.profile {
-  color: white;
-  text-decoration: underline;
-  position: absolute;
-  right: 80px;
-  padding: 10px;
 }
 
 .profilepic {
-  position: absolute;
-  right: 10px;
+  margin-right: 100px;
   border-radius: 50%;
   border: 1px solid white;
+  height: 32px;
+  width: 32px;
+}
+
+.loginprompt {
+  color: white;
 }
 
 </style>
