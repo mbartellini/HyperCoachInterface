72e9ffbb6d93912e481c30e67845d9aa73bbb84f
juan <juanigarcia@itba.edu.ar>
2021-10-12 17:49:54 -0300

    Change login prompt.

--- src/views/LoginPrompt.vue
+++ src/views/LoginPrompt.vue
@@ -1,24 +1,36 @@
 <template>
-  <v-card
-      elevation="24"
-      class="mx-auto mt-5"
-      width="550"
-      rounded
-  >
-    <v-card-title class="justify-center" > Parece que no iniciaste sesión :(</v-card-title>
-    <v-card-text class="text-center">
-      Para acceder a esta funcionalidad primero debe registrarse o iniciar sesión.
-    </v-card-text>
-    <v-list>
-      <v-list-item>
-        <v-btn class="primary no-uppercase" rounded @click="login"> Iniciar sesión </v-btn>
-      </v-list-item>
-      <v-list-item >
-        <router-link to="Login"> Registrarse </router-link>
-      </v-list-item>
-    </v-list>
-    <!--  TODO: fix button centering  -->
-  </v-card>
+  <div class="text-center">
+    <v-sheet
+        class="px-7 pt-7 pb-4 mt-5 text-center d-inline-block rounded-lg"
+        outlined
+        elevation="10"
+    >
+      <div class="text-h6 mb-4">
+        No has iniciado sesión :(
+      </div>
+      <div class="text-body-2 mb-4">
+        Para acceeder a esta funcionalidad debes iniciar sesión.
+      </div>
+
+      <div class="text-decoration-underline">
+        <v-btn
+            class="ma-2"
+            color="primary"
+            :to="{name: 'Login'}"
+        >
+          Iniciar sesión
+        </v-btn>
+
+        <v-btn
+            class="ma-2"
+            plain
+            :to="{name: 'Registration'}"
+        >
+          Registrarme
+        </v-btn>
+      </div>
+    </v-sheet>
+  </div>
 </template>
 
 <script>
@@ -34,7 +46,5 @@ export default {
 </script>
 
 <style scoped>
-.no-uppercase {
-  text-transform: none;
-}
+
 </style>
