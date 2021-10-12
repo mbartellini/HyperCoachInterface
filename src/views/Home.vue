398b6d8a7290e8fb2635d84b1d758334cb89df8a
juan <juanigarcia@itba.edu.ar>
2021-10-12 10:57:22 -0300

    Change home to adapt to RoutineCard.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,65 +1,41 @@
 <template>
-  <v-container>
-
-    <h1>Recientes</h1>
-
+  <div>
+    <h1>Favorites</h1>
     <v-container fluid >
       <v-row dense >
-        <v-col
-            v-for="card in cards"
-            :key="card.title"
-        >
-
-          <RoutineCard :img_src="card.src">{{card.title}}</RoutineCard>
-
-
+        <v-col v-for="(routine, i) in favorites" :key="i">
+          <RoutineCard :routine="routine"/>
         </v-col>
       </v-row>
     </v-container>
-
-    <h1>Todas las rutinas</h1>
-
-
-    <v-container fluid>
-      <v-row
-          v-for="n in 3"
-          :key="n"
-          dense>
-        <v-col
-            v-for="card in cards"
-            :key="card.title"
-        >
-
-          <RoutineCard :img_src="card.src">{{card.title}}</RoutineCard>
-
-
-        </v-col>
-      </v-row>
-    </v-container>
-
-  </v-container>
+  </div>
 </template>
 
 
 
 <script>
-import RoutineCard from '../components/RoutineCard'
+import RoutineCard from '@/components/RoutineCard'
 
 export default {
-  name: 'Home',
+  name: 'Favorites',
 
   components: {
     RoutineCard,
   },
 
   data: () => ({
-    cards: [
-      { title: 'Abdominales', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg'},
-      { title: 'Tren Superior', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg'},
-      { title: 'Piernas', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg'},
-      { title: 'Yoga', src: 'https://i.pinimg.com/originals/25/49/82/25498264b4b0e7bd98587789c0e4ffaa.jpg'},
-      { title: 'Brazos', src: '../assets/arms.jpg' }
-    ],
   }),
+
+  computed: {
+    favorites() {
+      return [
+        { title: 'Abdominales', img_src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg'},
+        { title: 'Tren Superior', img_src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg'},
+        { title: 'Piernas', img_src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg'},
+        { title: 'Yoga', img_src: 'https://i.pinimg.com/originals/25/49/82/25498264b4b0e7bd98587789c0e4ffaa.jpg'},
+        { title: 'Brazos', img_src: '../assets/arms.jpg' }
+      ]
+    }
+  }
 }
 </script>
