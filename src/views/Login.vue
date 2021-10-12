2886aea7f33a75fbb59e9912409aba44755fab9f
juan <juanigarcia@itba.edu.ar>
2021-10-12 20:35:39 -0300

    Change login view style.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,52 +1,79 @@
 <template>
-  <div>
-    <p v-show="error" class="text-sm text-red-500">{{ errorMsg }}</p>
+  <div class="text-center">
     <v-container fluid>
-      <v-col cols="8" sm="6" md="3" align="center" offset="4">
-        <h1>Iniciar sesión</h1>
-      </v-col>
-      <form @submit="login">
-<!--            <h1 class="text-left font-bold mb-5 font-montserrat">Username</h1>-->
-<!--            <input type="text" v-model="username"/>-->
-        <v-col cols="8" sm="6" md="3" align="center" offset="4">
-          <v-text-field v-model="username" outlined label="Usuario" clearable></v-text-field>
-        </v-col>
-<!--            <h1 class="text-left font-bold mb-5 font-montserrat">Password</h1>-->
-<!--            <input type="password" v-model="password">-->
-        <v-col cols="8" sm="6" md="3" align="center" offset="4">
-          <v-text-field
-              v-model="password"
-              :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-              :type="show ? 'text' : 'password'"
-              name="input-10-2"
-              label="Contraseña"
+      <v-row class="justify-left mb-3">
+        <GoBackButton/>
+      </v-row>
+      <v-sheet
+          class="px-7 pt-7 pb-4 mt-5 text-center d-inline-block rounded-lg"
+          outlined
+          elevation="10"
+      >
+        <div class="text-h6 mb-4">
+          Iniciar sesión
+        </div>
+
+        <v-text-field
+            v-model="username"
+            outlined label="Usuario"
+            clearable />
+
+        <v-text-field
+            v-model="password"
+            :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="show ? 'text' : 'password'"
+            name="input-10-2"
+            label="Contraseña"
+            outlined
+            @click:append="show = !show"
+        />
+
+        <div class="text-decoration-underline">
+          <v-btn
+              class="ma-2"
+              color="primary"
+              :to="{name: 'Login'}"
+          >
+            Iniciar sesión
+          </v-btn>
+
+          <v-btn
+              class="ma-2"
+              plain
               outlined
-              @click:append="show = !show"
-          ></v-text-field>
-        </v-col>
-<!--          <button type="submit" :disabled="password.length < 3" class="bg-green-400 p-5 text-white">-->
-<!--            Iniciar Sesión-->
-<!--          </button>-->
-        <v-col cols="2" offset="4" align="center">
-        <v-btn class="primary no-uppercase" rounded @click="login"> Iniciar sesión </v-btn>
-          <p class="my-2">
-            <router-link to="" >Olvidaste tu contraseña?</router-link>
-          </p>
-        </v-col>
-<!--        TODO: center this properly.      -->
-      </form>
+              :to="{name: 'Registration'}"
+          >
+            Registrarme
+          </v-btn>
+        </div>
+        <div class="text-decoration-underline">
+          <v-btn
+              class="ma-2"
+              plain
+              fab
+              :to="{name: 'Registration'}"
+          >
+            Olvidé mi contraseña
+          </v-btn>
+        </div>
+      </v-sheet>
     </v-container>
   </div>
 </template>
+
 <script>
 
 
 import {mapState, mapGetters, mapActions} from 'vuex'
 import {LoginCredentials} from "@/api/user";
 import router from "@/router";
+import GoBackButton from "@/components/GoBackButton";
 
 export default {
   name: 'Login',
+  components: {
+    GoBackButton,
+  },
 
   data() {
     return {
@@ -91,10 +118,5 @@ export default {
 
 input[type="text"] { padding: 2px; border: 4px solid gray }
 input[type="password"] { padding: 2px; border: 4px solid gray }
-
- .no-uppercase {
-   text-transform: none;
-
- }
 
 </style>
