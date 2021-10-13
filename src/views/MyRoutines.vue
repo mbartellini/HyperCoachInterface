2f104c294472d28bac379882d677fc0a6e147db2
juan <juanigarcia@itba.edu.ar>
2021-10-13 00:38:02 -0300

    Add create button.

--- src/views/MyRoutines.vue
+++ src/views/MyRoutines.vue
@@ -5,9 +5,16 @@
         <h1>Mis rutinas</h1>
       </v-row>
       <v-row class="text-h6 ma-3 pt-5">
-        <p>
-          ¡Estas son las rutinas que creaste!
-        </p>
+        <v-h6>
+          ¡Estas son las rutinas que creas!
+        </v-h6>
+        <v-spacer></v-spacer>
+        <v-btn
+            rounded
+            color="success"
+        ><v-icon left outlined> mdi-plus </v-icon>
+          <div class="text-decoration-underline">Crear</div>
+        </v-btn>
       </v-row>
       <v-row fluid>
         <RoutinesCardsGrid :routines="myRoutines" />
@@ -30,6 +37,16 @@ export default {
 
   data: () => ({
     myRoutines: [
+      { title: 'Abdominales', img_src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg'},
+      { title: 'Tren Superior', img_src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg'},
+      { title: 'Piernas', img_src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg'},
+      { title: 'Yoga', img_src: 'https://i.pinimg.com/originals/25/49/82/25498264b4b0e7bd98587789c0e4ffaa.jpg'},
+      { title: 'Brazos', img_src: '../assets/arms.jpg' },
+      { title: 'Abdominales', img_src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg'},
+      { title: 'Tren Superior', img_src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg'},
+      { title: 'Piernas', img_src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg'},
+      { title: 'Yoga', img_src: 'https://i.pinimg.com/originals/25/49/82/25498264b4b0e7bd98587789c0e4ffaa.jpg'},
+      { title: 'Brazos', img_src: '../assets/arms.jpg' },
       { title: 'Abdominales', img_src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg'},
       { title: 'Tren Superior', img_src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg'},
       { title: 'Piernas', img_src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg'},
